3f3d2dbf7bccd677b41b23a3243aafc62b19deae
h-yusuf <hafiy288@gmail.com>
2023-08-25 23:32:43 +0700

    add file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roboto"  : "roboto",
+        "poppins" : "poppins"
+      }
+    },
   },
   plugins: [],
 }
